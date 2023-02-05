78e86e6b847c8cca8f4b1c2adfcb9db238f23fc1
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-02-06 00:04:32 +0700

    Update 90_wifi.sh

--- aarch64_cortex-a53/90_wifi.sh
+++ aarch64_cortex-a53/90_wifi.sh
@@ -1,16 +1,11 @@
 #!/bin/sh
-
-  uci set wireless.@wifi-device[0].disabled=0
-  uci set wireless.@wifi-iface[0].device=radio0
-  uci set wireless.@wifi-iface[0].mode=ap
-  uci set wireless.@wifi-iface[0].ssid=AkariAmlogic
-  uci set wireless.@wifi-iface[0].network=lan
-  uci set wireless.@wifi-iface[0].encryption=psk2
-  uci set wireless.@wifi-iface[0].key=internet
-  uci set wireless.radio0.htmode='VHT20'
-  uci set wireless.radio0.band='5g'
-  uci set wireless.radio0.channel='128'
-  uci set wireless.radio0.cell_density='0'
-  uci commit wireless
+uci del wireless.default_radio0.disabled
+uci del wireless.radio0.disabled
+uci set wireless.radio0.htmode='VHT20'
+uci set wireless.radio0.cell_density='0'
+uci set wireless.default_radio0.ssid='OpenWrt-Amlogic'
+uci set wireless.default_radio0.encryption='psk2'
+uci set wireless.default_radio0.key='internet'
+uci commit wireless
 
 exit 0
