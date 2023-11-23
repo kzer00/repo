f7a7658b3eb72ec8b5226e51881ce24c0c369964
kzer00 <121999836+kzer00@users.noreply.github.com>
2023-11-23 22:24:03 +0700

    Update 30_pizero2.sh

--- aarch64_cortex-a53/30_pizero2.sh
+++ aarch64_cortex-a53/30_pizero2.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
-  uci set firewall.@zone[1].network='wan wan6 hilink tethering '
+  uci set firewall.@zone[1].network='wan wan6 hilink tethering fibocom '
   uci set firewall.@defaults[0].forward='ACCEPT'
   uci set firewall.@zone[1].input='ACCEPT'
   uci set firewall.@zone[1].forward='ACCEPT'
   uci commit firewall
 
   uci del network.wan6
+  uci del network.cfg030f15.ports
   uci set network.hilink=interface
   uci set network.hilink.device='eth0'
   uci set network.hilink.proto='dhcp'
   uci set network.tethering=interface
   uci set network.tethering.device='usb0'
   uci set network.tethering.proto='dhcp'
+  uci set network.fibocom=interface
+  uci set network.fibocom.proto='xmm'
   uci del network.cfg030f15.ports
   uci add_list network.cfg030f15.ports='end0'
   uci add_list network.lan.dns='8.8.8.8'
@@ -20,14 +23,14 @@
   uci commit network
 
   uci set system.@system[0]=system 
-  uci set system.@system[0].hostname=KZERO
+  uci set system.@system[0].hostname=Pizero2
   uci set system.@system[0].zonename=Asia/Jakarta
   uci set system.@system[0].timezone=WIB-7
   uci commit system
 
   uci set dhcp.domain=domain
-  uci set dhcp.domain.ip=192.168.10.1
-  uci set dhcp.domain.name=KZERO
+  uci set network.lan.ipaddr='192.168.10.1'
+  uci set dhcp.domain.name=Pizero2
   uci commit dhcp
   chmod +x /etc/profile
   chmod +x /usr/bin/sysinfo
